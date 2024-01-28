2bea4e4bef3447f361c2bd6dbbfba901725d1f30
abimanyu1510 <asalabimanyu15@gmail.com>
2024-01-28 23:30:30 +0530

    task completed

---
+++ index.js
@@ -0,0 +1,43 @@
+import { format } from "date-fns";
+import express from "express";
+import fs from 'fs';
+import path from 'path';
+
+
+const PORT=8000
+const app=express();
+
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+try {
+    let date=format(new Date(),'dd-MM-yyyy-HH-mm-ss')
+
+ fs.writeFileSync(`Current_TimeStamp/${date}.txt`,`${date}`,"utf8")
+ let data=fs.readFileSync(`Current_TimeStamp/${date}.txt`,"utf8")
+
+res.status(200).send(data)
+} catch (error) {
+    res.status(500).send("Internal Server Error")
+}
+
+})
+app.get("/timefiles",(req,res)=>{
+
+const folder='Current_TimeStamp';
+fs.readdir(folder,(err,e)=>{
+if (err) {
+    console.log(err);
+    res.status(500).send('listening files error')
+    
+} else {
+   const  textFiles=e.filter((file)=>path.extname(file)===".txt");
+   res.status(200).json(textFiles);
+}
+
+})
+
+
+})
+
+app.listen(PORT,()=>console.log(`App is Listening to ${PORT}`))
---
+++ readme.md
@@ -0,0 +1,4 @@
+<h1>NodeJS File System Task</h1>
+
+<h3>Write a API endpoint which will create a text file in a particular folder</h3>
+<h3>Write a API endpoint retrieve all text files in that particular folder</h3>
